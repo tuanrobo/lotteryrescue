ae612fd8ef951becd28958fdc56708c5dff95971
tuanrobo <tuanrobo@gmail.com>
2020-04-13 11:03:09 +0700

    update

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,19 @@
 $(function () {
-	$('[data-toggle="tooltip"]').tooltip();
+	$('[data-toggle="tooltip"]').tooltip();	
 });
+
+// fetch('https://randomuser.me/api/?results=10')
+// .then((e) => {
+//   return e.json();
+// }).then((x) => {
+//   const results = x.results;
+//   let data
+//   results.forEach((user) => {
+//     data += "<li><a href='"+user.picture.large+"' target='_blank'>Picture</a> - "+user.name.first+" "+user.name.last+"</li><br>";
+//   });
+
+  
+//   const p = document.querySelector("#users0d");  
+//   p.innerHTML = data;
+// })
+
