159ab6eb0d8055c5886733bb179a65b0c810085b
tuanrobo <tuanrobo@gmail.com>
2020-04-29 10:08:07 +0700

    update UI

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,4 +1,29 @@
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip();	
+	
+	var marginTop = function () {
+		$('main').each(function () {
+			var header = $('header').height();
+			// var main = $('main').height();
+			$(this).css({
+				'top': header,
+				// overflow: 'hidden'
+			})
+			// if (bannerHeight < innerHeight) {
+			// 	$(this).css({
+			// 		'height': 'auto',
+			// 		'padding-top': '20px',
+			// 		'padding-bottom': '20px',
+			// 	})
+			// } else (
+			// 	$(this).css({
+			// 		'margin-top': bannerHeight,
+			// 		// overflow: 'hidden'
+			// 	})
+			// )
+		})
+	}
+	
+	$(window).on("load", marginTop)
+	$(window).on("resize", marginTop)
 });
-
